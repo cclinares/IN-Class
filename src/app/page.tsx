eec6f4632279f212d06636717b59c418ae3d0825
cclinares <ivanjozape@gmail.com>
2025-04-02 19:15:37 -0300

    Corrijo rutas y estructura para desplegar en Vercel

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-﻿export default function HomePage() {
-  return (
-    <main style={{ padding: '2rem' }}>
-      <h1>Bienvenido al sistema educativo</h1>
-      <p>Por favor, dirígete al <a href="/login">login</a> para ingresar.</p>
-    </main>
-  );
-}
