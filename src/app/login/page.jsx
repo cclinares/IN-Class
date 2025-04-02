79894a9cdf8e481a44d85cced558512dae094de8
cclinares <ivanjozape@gmail.com>
2025-04-02 18:26:31 -0300

    Fix: subir login/page.jsx a producción

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,9 +1,11 @@
 ﻿// src/app/login/page.jsx
 
+'use client';
+
 export default function LoginPage() {
   return (
-    <div>
-      <h1>Login IN Class</h1>
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">Login IN Class</h1>
     </div>
   );
 }
