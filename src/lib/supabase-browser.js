3e4618d19eb98502acf7400665e661ba11c2fd87
cclinares <ivanjozape@gmail.com>
2025-04-09 14:00:30 -0400

    Corrige error de sesión usando Supabase con createBrowserClient

---
+++ src/lib/supabase-browser.js
@@ -0,0 +1,7 @@
+// src/lib/supabase-browser.js
+import { createBrowserClient } from '@supabase/ssr'
+
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
