dd0b338ffc2404e9c424433f8c0aec9887bf5708
cclinares <ivanjozape@gmail.com>
2025-04-09 16:59:36 -0400

    Agrega export default correcto para supabase-browser

--- src/lib/supabase-browser.js
+++ src/lib/supabase-browser.js
@@ -1,7 +1,8 @@
-// src/lib/supabase-browser.js
-import { createBrowserClient } from '@supabase/ssr'
+import { createBrowserClient } from "@supabase/ssr";
 
-export const supabase = createBrowserClient(
+const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
+
+export default supabase;
